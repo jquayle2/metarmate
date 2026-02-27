362960c278949f51131017ed8b4fe276f6be79f6
Jeff Quayle <jquayle@gmail.com>
2026-02-27 12:56:35 -0800

    Initial Commit

---
+++ MetarMate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MetarMate
+//
+//  Created by jquayle on 2/27/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
